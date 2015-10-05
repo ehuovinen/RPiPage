e4b6c7ae49784fd27fb44c35e0c217cc8a20677b
root <root@raspberrypi.(none)>
2015-10-05 13:21:53 +0300

    progress

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 session_start();
+/*
 if(!array_key_exists('smHandle',$_SESSION)){
 	$_SESSION['smHandle']=shmop_open(0x3000,'a',0,0);
 	if ($_SESSION['smHandle']==False){
@@ -8,42 +9,114 @@ if(!array_key_exists('smHandle',$_SESSION)){
 		die("control program is not running");
 	}
 }
+*/
 
 echo
 "
 <!DOCTYPE HTML>
 <html manifest='cache.manifest'>
 <head>
+<meta http-equiv='refresh' content='10'>
 </head>
 <style>
+#tLightBox{
+	width : 100px;
+	height : 300px;
+}
 </style>
 <body>
 ";
+if(array_key_exists('c',$_GET)){
+	$command="";
+	switch ($_GET["c"]){
+		case "sr":
+			$command="setRed\n";
+			break;
+		case "rr":
+			$command="resetRed\n";
+			break;
+		case "sy":
+			$command="setYellow\n";
+			break;
+		case "ry":
+			$command="resetYellow\n";
+			break;
+		case "sg":
+			$command="setGreen\n";
+			break;
+		case "rg":
+			$command="resetGreen\n";
+			break;
+		default:
+			$command="unrecognizedGet\n";
+			break;
+	}
+	unset($_GET);
+	$out = fopen("/tmp/commandPipe","w");
+	fwrite($out,$command);
+	fclose($out);
+	header("Location: index.php");
+	exit();
+}
+
 $handle=shmop_open(0x3000,'a',0,0);
 $rv=shmop_read($handle,0,10);
 if ($rv[0]=="T"){
-	echo '<p>RED
-<svg width="100" height="100">
-<a xlink:href="http://www.google.com">
-	<circle cx="50" cy="50" r="30" stroke="black" stroke-width="1" fill="red">
-<a>
-</svg>
-';
-}else{echo '<p>not red
-<svg width="100" height="100">
-<a xlink:href="http://www.google.com">
-	<circle cx="50" cy="50" r="30" stroke="black" stroke-width="1" fill="gray">
-<a>
-</svg>
-';}
+	echo '
+		<div id="tLightBox">
+		<svg width="100" height="80">
+		<a xlink:href="?c=rr">
+		<circle cx="50" cy="40" r="30" stroke="black" stroke-width="1" fill="red">
+		</a>
+		</svg>
+	';
+}else{
+	echo '
+		<div id="tLightBox">
+		<svg width="100" height="80">
+		<a xlink:href="?c=sr">
+		<circle cx="50" cy="40" r="30" stroke="black" stroke-width="1" fill="gray">
+		</a>
+		</svg>
+	';
+}
 
 if ($rv[1]=="T"){
-	echo '<p>YELLOW';
-}else{echo '<p>not yellow';}
+	echo '
+		<svg width="100" height="80">
+		<a xlink:href="?c=ry">
+		<circle cx="50" cy="40" r="30" stroke="black" stroke-width="1" fill="yellow">
+		</a>
+		</svg>
+	';
+}else{
+	echo '
+		<svg width="100" height="80">
+		<a xlink:href="?c=sy">
+		<circle cx="50" cy="40" r="30" stroke="black" stroke-width="1" fill="gray">
+		</a>
+		</svg>
+	';
+}
 
 if ($rv[2]=="T"){
-	echo '<p>GREEN';
-}else{echo '<p>not green';}
+	echo '
+		<svg width="100" height="80">
+		<a xlink:href="?c=rg">
+		<circle cx="50" cy="40" r="30" stroke="black" stroke-width="1" fill="lime">
+		</a>
+		</svg>
+	';
+}else{
+	echo '
+		<svg width="100" height="80">
+		<a xlink:href="?c=sg">
+		<circle cx="50" cy="40" r="30" stroke="black" stroke-width="1" fill="gray">
+		</a>
+		</svg>
+</div>
+	';
+}
 
 echo"<p><a href='logout.php'>Logout</a>";
 ?>
